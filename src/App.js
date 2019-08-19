7a43ddb0274086ea14bac39a5577aef118378e21
vamsi <Vamsi.Karanam@rgare.com>
2019-08-19 11:22:02 -0500

    Included Search Option - working code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 
 import { CardList } from './components/card-list/card-list.component';
-//import { SearchBox } from './components/search-box/search-box.component';
 
 import './App.css';
+import {SearchBox} from "./components/search-box/search-box.component";
 
 class App extends Component {
   constructor() {
@@ -21,19 +21,23 @@ class App extends Component {
         .then(users => this.setState({ monsters: users }));
   }
 
-  onSearchChange = event => {
-    this.setState({ searchField: event.target.value });
+  handleChange = e => {
+    this.setState({searchField: e.target.value})
   };
 
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter(monster =>
-        monster.name.toLowerCase().includes(searchField.toLowerCase())
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
     );
 
     return (
         <div className='App'>
           <h1>Monsters Rolodex</h1>
+          <SearchBox
+            placeholder='Search Monsters'
+            handleChange={this.handleChange}
+          />
           <CardList monsters={filteredMonsters} />
         </div>
     );
